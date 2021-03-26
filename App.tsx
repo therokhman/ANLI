0cf73827c98fff6662bd2431800d526ef9aaeb57
alex_rokhman <alex.bobr@rambler.ru>
2021-03-26 19:35:08 +0300

    add notes and notifications

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import ProfileContainer from "./screens/profile/ProfileContainer";
 import ProgramsContainer from "./screens/programs/ProgramsContainer";
 import ModesContainer from "./screens/modes/ModesContainer";
+import Notifications from "./screens/Notifications"
 
 export default function App() {
     return (
@@ -17,6 +18,7 @@ export default function App() {
                 <Stack.Screen name="modesContainer" component={ModesContainer} options={{ headerShown: false }} />
                 <Stack.Screen name="hygiene" component={Hygiene} options={{ title: 'Гигиена полости рта', headerBackTitleVisible: false  }} />
                 <Stack.Screen name="profileContainer" component={ProfileContainer} options={{ headerShown: false }} />
+                <Stack.Screen name="notifications" component={Notifications} options={{ title: 'Мои уведомления' }} />
             </Stack.Navigator>
         </NavigationContainer>
     );
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -4,7 +4,10 @@ import MyButton from "../MyButton";
 
 const Home = (props: HomeProps) => {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-between', paddingTop: 30, paddingBottom: 40 }}>
+            <View style={{ width: "90%", display: "flex", alignItems: 'stretch', justifyContent: 'space-between' }}>
+                <MyButton text={'Мои уведомления'} onPress={() => {props.navigation.navigate('notifications')}} />
+            </View>
             <View style={{ width: "90%", display: "flex", flexDirection: "row", alignItems: 'stretch', justifyContent: 'space-between' }}>
                 <MyButton text={'Мой \nПрофиль'} onPress={() => {props.navigation.navigate('profileContainer')}} />
                 <MyButton text={'Режимы'} onPress={() => {props.navigation.navigate('modesContainer')}} />
