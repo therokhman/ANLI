08f1aca58e40d59abe8ce56446b82eccd0727bc3
alex_rokhman <alex.bobr@rambler.ru>
2021-03-26 14:47:08 +0300

    add modes and programs

--- App.tsx
+++ App.tsx
@@ -6,6 +6,13 @@ import Profile from "./screens/profile/Profile";
 import Hygiene from "./screens/hygiene/Hygiene";
 import Programs from "./screens/programs/Programs";
 import Modes from "./screens/modes/Modes";
+import Standard from "./screens/modes/Standard";
+import Martaller from "./screens/modes/Martaller";
+import KAI from "./screens/modes/KAI";
+import Braces from "./screens/programs/Braces";
+import Fresh from "./screens/programs/Fresh";
+import Gumprotect from "./screens/programs/Gumprotect";
+import Teethwhite from "./screens/programs/Teethwhite";
 
 import MyButton from './MyButton'
 import { NavigationContainer } from '@react-navigation/native';
@@ -13,25 +20,24 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
     return (
-        <NavigationContainer >
+        <NavigationContainer>
             <Stack.Navigator initialRouteName="home">
                 <Stack.Screen name="home" component={Home} options={{ title: 'Главная' }} />
                 <Stack.Screen name="programs" component={Programs} options={{ title: 'Специальные программы', headerBackTitleVisible: false }} />
                 <Stack.Screen name="modes" component={Modes} options={{ title: 'Режимы', headerBackTitleVisible: false }} />
                 <Stack.Screen name="hygiene" component={Hygiene} options={{ title: 'Гигиена полости рта', headerBackTitleVisible: false  }} />
                 <Stack.Screen name="profile" component={Profile} options={{ title: 'Мой профиль', headerBackTitleVisible: false }} />
+                <Stack.Screen name="standard" component={Standard} options={{ title: 'Стандартный метод', headerBackTitleVisible: false }} />
+                <Stack.Screen name="martaller" component={Martaller} options={{ title: 'Метод Марталлера', headerBackTitleVisible: false }} />
+                <Stack.Screen name="kai" component={KAI} options={{ title: 'Метод KAI', headerBackTitleVisible: false }} />
+                <Stack.Screen name="braces" component={Braces} options={{ title: 'Уход за брекет-системой', headerBackTitleVisible: false }} />
+                <Stack.Screen name="fresh" component={Fresh} options={{ title: 'Свежесть дыхания', headerBackTitleVisible: false }} />
+                <Stack.Screen name="gumprotect" component={Gumprotect} options={{ title: 'Защита десен', headerBackTitleVisible: false }} />
+                <Stack.Screen name="teethwhite" component={Teethwhite} options={{ title: 'Отбеливание зубов', headerBackTitleVisible: false }} />
             </Stack.Navigator>
         </NavigationContainer>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
-});
-
+// @ts-ignore
 const Stack = createStackNavigator();
