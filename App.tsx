c6cc54deaa1938eec0e7ba9af309a35261f0aed8
Alexey Raketski <raketskij@gmail.com>
2021-03-31 01:20:10 +0300

    feat: Journal 2

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import 'react-native-gesture-handler';
-import React, {createContext} from 'react';
+import React from 'react';
 import Home from "./screens/Home";
 import Hygiene from "./screens/hygiene/Hygiene";
 import {NavigationContainer} from '@react-navigation/native';
@@ -8,15 +8,10 @@ import ProfileContainer from "./screens/profile/ProfileContainer";
 import ProgramsContainer from "./screens/programs/ProgramsContainer";
 import ModesContainer from "./screens/modes/ModesContainer";
 import Notifications from "./screens/Notifications"
-import JournalModel from "./screens/profile/JournalModel";
-import {ActionsEnum} from "./screens/profile/ActionsEnum";
-import {JournalSettings} from "./screens/profile/JournalSettings";
-import {JournalContext} from "./JournalContext";
 
 export default function App() {
-    const [model, setModel] = React.useState(new JournalModel({}, {[ActionsEnum.rinsing]: 3} as JournalSettings));
     return (
-        <JournalContext.Provider value={{ model, setModel}}>
+
             <NavigationContainer>
                 <Stack.Navigator initialRouteName="home">
                     <Stack.Screen name="home" component={Home} options={{title: 'Главная', headerStyle: {backgroundColor: '#83aba4'}}}/>
@@ -29,7 +24,6 @@ export default function App() {
                     <Stack.Screen name="notifications" component={Notifications} options={{title: 'Мои уведомления', headerBackTitleVisible: false, headerStyle: {backgroundColor: '#83aba4'}}}/>
                 </Stack.Navigator>
             </NavigationContainer>
-        </JournalContext.Provider>
     );
 }
 
--- screens/profile/JournalContent.tsx
+++ screens/profile/JournalContent.tsx
@@ -2,30 +2,24 @@ import React from 'react';
 import {Image, Text, View} from "react-native";
 import {JournalMode} from "./JournalModeEnum";
 import JournalButton from "./JournalButton";
-import teeth from '../../assets/teeth.png';
-import JournalModel from "./JournalModel";
 import {ActionsEnum} from "./ActionsEnum"
-import {JournalSettings} from "./JournalSettings";
 
 const JournalContent = ({route}: any) => {
     const mode: JournalMode = route.params.mode
     console.log('init', mode);
 
     return (
-        <View style={{paddingLeft: 20, paddingRight: 20, paddingTop: 70, alignItems: 'center'}}>
-            <Text>
-                <Text style={{fontWeight: 'bold'}}>{mode}</Text>
-            </Text>
+        <View style={{paddingLeft: 20, paddingRight: 20, paddingTop: '40%', alignItems: 'center'}}>
             <View>
                 <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                    <JournalButton text={'test'} image={teeth} mode={mode} action={ActionsEnum.morningCleaning} />
-                    <JournalButton text={'test'} image={teeth} mode={mode} action={ActionsEnum.noonCleaning} />
-                    <JournalButton text={'test'} image={teeth} mode={mode} action={ActionsEnum.nightCleaning} />
+                    <JournalButton text={'test'} image={require('../../assets/teeth.png')} mode={mode} action={ActionsEnum.morningCleaning} />
+                    <JournalButton text={'test'} image={require('../../assets/teeth.png')} mode={mode} action={ActionsEnum.noonCleaning} />
+                    <JournalButton text={'test'} image={require('../../assets/teeth.png')} mode={mode} action={ActionsEnum.nightCleaning} />
                 </View>
                 <View style={{flexDirection: 'row', justifyContent: 'space-between', marginTop: 20}}>
-                    <JournalButton text={'test'} image={teeth} mode={mode} action={ActionsEnum.rinsing} />
-                    <JournalButton text={'test'} image={teeth} mode={mode} action={ActionsEnum.tongueCleaning} />
-                    <JournalButton text={'test'} image={teeth} mode={mode} action={ActionsEnum.flossing} />
+                    <JournalButton text={'test'} image={require('../../assets/teeth.png')} mode={mode} action={ActionsEnum.rinsing} />
+                    <JournalButton text={'test'} image={require('../../assets/teeth.png')} mode={mode} action={ActionsEnum.tongueCleaning} />
+                    <JournalButton text={'test'} image={require('../../assets/teeth.png')} mode={mode} action={ActionsEnum.flossing} />
                 </View>
             </View>
         </View>
